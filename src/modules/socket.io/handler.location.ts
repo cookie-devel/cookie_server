e44d536660bc05a541aeacc68a4ac7cb2f743bba
unknown <elic121@ajou.ac.kr>
2023-10-17 16:21:36 +0900

    <modified> handler.location

--- src/modules/socket.io/handler.location.ts
+++ src/modules/socket.io/handler.location.ts
@@ -1,6 +1,5 @@
 import { Namespace, Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
-import LocationModel from "@/schemas/map/location.model";
 import * as MapType from "@/interfaces/map";
 import { MapEvents } from "@/interfaces/map";
 import Account from "@/schemas/account.model";
@@ -22,27 +21,25 @@ export default (
       async ({ latitude, longitude }: MapType.MapRequest) => {
         console.log(`${socket.data.userID} ${MapEvents.position}`);
         try {
-          const location = await LocationModel.create({
-            latitude,
-            longitude,
-          });
-          console.log("location", location);
+          console.log("location", latitude, longitude);
 
           const userInfo = await Account.findById(socket.data.userID).exec();
-          const friendList = userInfo.getFriends();
+          const friendList = userInfo.friendIDs;
 
-          // console.log("friendList", friendList);
+          friendList.forEach((friendID) => {
+            // socket.join(friendID);
+            socket.to(friendID).emit(MapEvents.position, {
+              userid: socket.data.userID,
+              latitude: latitude,
+              longitude: longitude,
+            } as MapType.MapResponse);
+          });
+
           // socket.emit(MapEvents.position, {
           //   userid: socket.data.userID,
           //   latitude: location.latitude,
           //   longitude: location.longitude,
           // } as MapType.MapResponse);
-
-          socket.to(await friendList).emit(MapEvents.position, {
-            userid: socket.data.userID,
-            latitude: location.latitude,
-            longitude: location.longitude,
-          } as MapType.MapResponse);
         } catch (error) {
           console.log(error);
         }
